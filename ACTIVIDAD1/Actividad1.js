c20f9889d5ea603b7fc2bf5b2b167333e67a6263
Cristopher <yiissuuss@gmail.com>
2022-05-31 19:48:35 -0500

    Actividad1

---
+++ ACTIVIDAD1/Actividad1.js
@@ -0,0 +1,22 @@
+//Ejercicio 1
+numero=prompt("Ingresa un valor numerico")
+resultado=numero*2
+console.log(resultado)
+
+//Ejercicio 2
+numero1=prompt("Primer numero")
+numero2=prompt("Segundo numero")
+resta=numero1-numero2
+console.log(resta)
+
+//Ejercicio 3
+var r=5.3
+const pi=3.14
+areaCirculo=pi*(r**2)
+console.log(areaCirculo)
+
+//Ejercicio 4
+const D=8
+const d=6
+areaRombo=(D*d)/2
+console.log(areaRombo)
