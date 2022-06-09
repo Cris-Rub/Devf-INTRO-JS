f9aca37add913e4ddd7cc348f93d0fce15c56f87
Cristopher <yiissuuss@gmail.com>
2022-06-09 11:46:06 -0500

    Actividad 10

---
+++ ACTIVIDAD8/Actividad8.js
@@ -0,0 +1,41 @@
+/**
+ * Del siguiente objeto:
+    var student = {
+        name: "David Rayy",
+        age: 34,
+        job: "engineer",
+        hobbies: ['Sky', 'football', 'walking my dog']
+    };
+ * 
+    1. Muestra en pantalla el valor de la propiedad name mediante dot notation
+    2. Muestra en pantalla el valor de la propiedad age mediante bracket notation
+    3. Modifica el valor de la propiedad name y que el nuevo valor sea: "Steve Jobs"
+ */
+
+var student = {
+    name: "David Rayy",
+    age: 34,
+    job: "engineer",
+    hobbies: ['Sky', 'football', 'walking my dog']
+};
+console.log(student.name);
+console.log(student['age']);
+student.name="Steve Jobs";
+
+/**
+ * Escriba un programa de JavaScript para enumerar las propiedades de un objeto de JavaScript.
+    Objeto de muestra:
+
+    var student = {
+        name: "David Rayy",
+        sclass: "VI",
+        rollno: 12
+    };
+ */
+
+var student = {
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12
+};
+
